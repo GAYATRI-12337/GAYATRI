4b8f17d0df81195ea50ac2a1dc8c8c467130e9e4
GAYATRI-12337 <158122444+GAYATRI-12337@users.noreply.github.com>
2024-02-21 15:06:50 +0530

    Create boxVolume.h

---
+++ boxVolume.h
@@ -0,0 +1,15 @@
+#include<iostream>
+
+Dept. of CSE, MVGR College of Engineering(Autonomous)
+
+21
+
+Object Oriented Programming Lab Roll Number: 22331A0519
+
+using namespace std;
+#define VOLUME
+void volume(float l,float b,float h)
+{
+float volume=l*b*h;
+cout<<"The volume of the box : "<<volume<<endl;
+}
