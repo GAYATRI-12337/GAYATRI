913f7b3c854377a5ce486a7fef97e2439cbcb11f
GAYATRI-12337 <158122444+GAYATRI-12337@users.noreply.github.com>
2024-05-01 20:53:28 +0530

    Create MapInterface.java

---
+++ MapInterface.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+public class MapInterface
+{
+public static void main(String[] args)
+{
+Map<String, Integer> h=new HashMap<>();
+h.put("Gayatri", 01);
+h.put("Mohan",27);
+System.out.println("Hash map is : "+h);
+// Iterating over key-value pairs
+System.out.println("Key-Value pairs :");
+for(Map.Entry<String, Integer> entry:h.entrySet())
+{
+System.out.println(entry.getKey() + " : " + entry.getValue());
+}
+}
+}
